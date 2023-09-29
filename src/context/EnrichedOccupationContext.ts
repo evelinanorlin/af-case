0d7de3498a53739587986b6930240d78346e9cb9
Evelina Norlin <evelina.norlin@hotmail.com>
2023-09-29 10:10:39 +0200

    fixed

--- src/context/EnrichedOccupationContext.ts
+++ src/context/EnrichedOccupationContext.ts
@@ -1,12 +1,21 @@
 import { createContext } from "react";
 import { IOccupation } from "../models/IOccupation";
-
-interface IAction {
-  type: string;
-  payload: IOccupation[];
-}
+import { IAction } from "../reducers/SSYKoccupationdescriptionReducer";
 
 export const EnrichedOccupationContext = createContext<{
+
   stateEnrichedOccupation: IOccupation[];
+
   dispatchEnrichedOccupation: React.Dispatch<IAction>;
-}>([]);
+
+}>({
+
+  stateEnrichedOccupation: [],
+
+  dispatchEnrichedOccupation: function (): void {
+
+    throw new Error("Function not implemented.");
+
+  }
+
+});
